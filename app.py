9238f25ec15c201c3b1424fbe72f6fa716b8474d
ktb702 <59974874+ktb702@users.noreply.github.com>
2021-01-22 07:21:11 -0700

    removed secret key

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-#in order to use csrf / forms you need a secret key
-app.config['SECRET_KEY'] = 'secret-key13534+8761'
+
+#secret key was defined here
 
 #routes must be imported after Flask is instantiated
 from routes import *
