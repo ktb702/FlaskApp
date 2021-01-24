0ae50a274d45254e18c9922e2b2a45a7c26217b9
ktb702 <59974874+ktb702@users.noreply.github.com>
2021-01-23 22:26:36 -0700

    Added SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 #secret key was defined here
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
 
 #routes must be imported after Flask is instantiated
 from routes import *
